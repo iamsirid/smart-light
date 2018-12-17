3eb73837c1a0a54eb8196bd064a6d97544bd3b2b
RiT <iamsirid@gmail.com>
2018-12-17 21:46:28 +0700

    add preflight

--- server.js
+++ server.js
@@ -39,7 +39,14 @@ let tempData = [
     isOn: 0
   }
 ];
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/api/get/data", (req, res) => {
   res.send(tempData);
 });
